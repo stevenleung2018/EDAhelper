93464446ae094588290c37542e2e619f0a0ebb59
Steven Leung <klsleung@student.ubc.ca>
2024-03-03 11:29:47 -0800

    Fix indents.

--- src/EDAhelper/column_stats.py
+++ src/EDAhelper/column_stats.py
@@ -2,31 +2,33 @@ import pandas as pd
 import numpy as np
 import statistics
 
+
 def column_stats(data, columns):
     """
-  Obtain summary statistics of column(s) including count, mean, median, mode, Q1, Q3, 
-  variance, standard deviation, correlation, and covariance in table format.
+    Obtain summary statistics of column(s) including count, mean, median, mode, Q1, Q3, 
+    variance, standard deviation, correlation, and covariance in table format.
 
-  Parameters
-  -------------
+    Parameters
+    -------------
 
-  data: array_like
-           The data set from which columns will be selected
+    data: array_like
+        The data set from which columns will be selected
 
-  columns: vector of strings
-            Columns for which to obtain summary stats, correlation matrix, and covariance matrix
-            (if > 1 column arguments used)
+    columns: vector of strings
+        Columns for which to obtain summary stats, correlation matrix, and covariance matrix
+        (if > 1 column arguments used)
 
-  Returns
-  -------------
-  array
-          Summary table detailing all statistics and correlations between chosen columns
+    Returns
+    -------------
+    array
+         Summary table detailing all statistics and correlations between chosen columns
 
-  Examples
-  -------------
-  >>> column_stats(iris, ('sepal_width', 'petal_length'))
-  >>>
-  """
+    Examples
+    -------------
+    >>> column_stats(iris, ('sepal_width', 'petal_length'))
+    >>>
+    """
+
     if not isinstance(columns, (list, tuple, np.ndarray)):
         raise TypeError("'columns' should be a list, tuple, or array")
     for column in columns:
@@ -34,13 +36,12 @@ def column_stats(data, columns):
             raise TypeError("item in columns should be of type string")
     if not isinstance(data, pd.DataFrame):
         raise TypeError("data should be a DataFrame")
-        
+
     for column in columns:
         for row in data[column]:
             if isinstance(row, str):
                 raise TypeError("values should be of type integer")
-    
-    
+
     statsdict = {'Column': [], 'Count': [], 'Mean': [], 'Median': [], 'Mode': [], 'Q1': [], 'Q3': [], 'Var': [], 'Stdev': []}
     for column in columns:
         statsdict['Column'].append(column)
@@ -52,7 +53,6 @@ def column_stats(data, columns):
         statsdict['Q3'].append(round(float(data[column].describe().loc['75%']), 3))
         statsdict['Var'].append(round(data[column].var(), 3))
         statsdict['Stdev'].append(round(data[column].std(), 3))
-        
 
     cols = []
     for column in columns:
